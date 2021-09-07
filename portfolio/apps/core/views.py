b3c77b9c3950d2c84787cb29b458c7cc31a4ed17
jquinterom <jfredyquintero@hotmail.com>
2021-09-07 16:32:52 -0500

    Se agrega la lista de objetos para los tipos de usuarios

--- portfolio/apps/core/views.py
+++ portfolio/apps/core/views.py
@@ -4,7 +4,14 @@ from ..portfolio_.models import ProjectType
 
 
 def home(request):
-    # proyectos 
-    projects =  Project.objects.filter(active=1)
+    # proyectos
+    projects = Project.objects.filter(active=1)
 
-    return render(request, "core/base.html", {'projects': projects})
+    #types projects
+    types_project = []
+    for tp in projects:
+        exists = tp.type_project in types_project;
+        if(tp.type_project not in types_project):
+            types_project.append(tp.type_project)
+
+    return render(request, "core/base.html", {'projects': projects, 'types_project': types_project})
