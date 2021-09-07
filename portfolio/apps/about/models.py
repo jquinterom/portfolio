f7d5fb02ed45b1ba6cfad94417ca88402df33486
jquinterom <jfredyquintero@hotmail.com>
2021-09-07 17:13:25 -0500

    modelo de acerca de creado

--- portfolio/apps/about/models.py
+++ portfolio/apps/about/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class About(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre')
+    age = models.IntegerField(verbose_name='Edad')
+    email = models.EmailField(verbose_name='Correo')
+    country = models.CharField(verbose_name='País', max_length=50)
+    city = models.CharField(verbose_name='Ciudad', max_length=50)
+    description = models.TextField(verbose_name='Descripción')
+    curriculun_vitae = models.FileField(upload_to='files/curriculum/', verbose_name='CV')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
+
+    class Meta:
+        verbose_name = "Acerca de"
+        verbose_name_plural = "Acerca de"
+
+    def __str__(self):
+        return str(self.name)
+
