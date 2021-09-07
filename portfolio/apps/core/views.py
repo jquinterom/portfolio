a237a9eaeed0fb80de182ce88a37c1a0bc4a2437
jquinterom <jfredyquintero@hotmail.com>
2021-09-06 22:44:03 -0500

    Se agrega la funcionalidad de cargar las imagenes para el portafolio

--- portfolio/apps/core/views.py
+++ portfolio/apps/core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
 from ..portfolio_.models import Project
+from ..portfolio_.models import ProjectType
 # Create your views here.
 
 def home(request):
+    # tipo de proyectos
+    projectsTypes = ProjectType.objects.all()
+
+    # proyectos 
     projects =  Project.objects.all()
-    return render(request, "core/base.html", {'projects': projects})
+    return render(request, "core/base.html", {'projects': projects, 'projectsTypes': projectsTypes})
