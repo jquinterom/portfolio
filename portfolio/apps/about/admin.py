07af6b8023895048bedee3376f633418b07a1d73
jquinterom <jfredyquintero@hotmail.com>
2021-09-08 11:00:42 -0500

    Se cambia la forma de importar los modelos

--- portfolio/apps/about/admin.py
+++ portfolio/apps/about/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import About
-from .models import SocialNetwork
-from .models import Services
+from .models import About, SocialNetwork, Services
 
 
 class AboutAdmin(admin.ModelAdmin):
