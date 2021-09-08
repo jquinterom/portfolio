2413dc9298f18fd84b6c04df51808f64ab43283b
jquinterom <jfredyquintero@hotmail.com>
2021-09-08 10:59:49 -0500

    Se agrega resume al panel de administracion

--- portfolio/apps/resume/admin.py
+++ portfolio/apps/resume/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Education, Experience
 
-# Register your models here.
+
+class EducationAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+class ExperiencenAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(Education, EducationAdmin)
+admin.site.register(Experience, ExperiencenAdmin)
+
