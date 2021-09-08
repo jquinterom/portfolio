aabc019f3bf2188116d9d3cd9a9f861a107c075e
jquinterom <jfredyquintero@hotmail.com>
2021-09-08 15:17:30 -0500

    Se crea modelo blog y se integra con la interfaz

--- portfolio/apps/blog/models.py
+++ portfolio/apps/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+# Modelo de blog
+class Blog(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Título')
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name='Imagen', upload_to='blog/')
+    link = models.CharField(max_length=2000, verbose_name="Enlace", default='#', null=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
+
+    class Meta:
+        verbose_name = "Blog"
+        verbose_name_plural = "Blogs"
+
+
+    def __str__(self):
+        return str(self.title)
