a43088e67246372a31fb5637965e82d63c3f7665
Jhon Quintero <jfqm7710@gmail.com>
2024-11-01 18:21:51 -0500

    fix(exaustive): eslint disable next line react hooks exhaustive deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,6 +30,7 @@ export default function Home() {
 
     window.addEventListener('scroll', handleScroll)
     return () => window.removeEventListener('scroll', handleScroll)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
