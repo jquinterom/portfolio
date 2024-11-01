2a37276ac92d913b01a08c87bb8581f2116e0158
Jhon Quintero <jfqm7710@gmail.com>
2024-10-31 21:01:13 -0500

    refactor(favicon): deleting favicon and using custom title

--- src/app/components/TechIcon.tsx
+++ src/app/components/TechIcon.tsx
@@ -1,8 +1,17 @@
-export const TechIcon = ({ name, color, children }: { name: string, color: string, children: React.ReactNode }) => (
-    <div className="flex flex-col items-center">
-        <div className={`w-10 h-10 rounded-full ${color} flex items-center justify-center text-white mb-1`}>
-            {children}
+interface TechIconProps {
+    name: string;
+    color: string;
+    children: React.ReactNode;
+}
+
+export const TechIcon = ({ name, color, children }: TechIconProps) => {
+
+    return (
+        <div className="flex flex-col items-center">
+            <div className={`w-10 h-10 rounded-full ${color}  flex items-center justify-center text-white mb-1`}>
+                {children}
+            </div>
+            <span className="text-xs text-center">{name}</span>
         </div>
-        <span className="text-xs text-center">{name}</span>
-    </div>
-)
+    )
+}
